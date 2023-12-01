9fb4e1262179c2d3f42f250b35518cddb9f2270e
Amar Tauqeer <amar.tauqeer@gmail.com>
2023-12-01 17:40:43 +0100

    initial commit

---
+++ department-service/src/main/java/com/microservices/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,53 @@
+package com.microservices.departmentservice.controller;
+
+import com.microservices.departmentservice.client.EmployeeClient;
+import com.microservices.departmentservice.model.Department;
+import com.microservices.departmentservice.repository.DepartmentRepository;
+
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private EmployeeClient employeeClient;
+
+    @PostMapping
+    public Department add(@RequestBody Department department) {
+        LOGGER.info("Department add: {}", department);
+        return departmentRepository.addDepartment(department);
+    }
+
+    @GetMapping
+    public List<Department> findAll() {
+        LOGGER.info("Department find");
+        return departmentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable Long id) {
+        LOGGER.info("Department find: id={}", id);
+        return departmentRepository.findById(id);
+    }
+
+    @GetMapping("/with-employees")
+    public List<Department> findAllWithEmployees() {
+        LOGGER.info("Department from employee service find");
+
+        List<Department> departments = departmentRepository.findAll();
+        departments.forEach(department -> department.setEmployees(employeeClient.findByDepartmentId(department.getId())));
+        return departments;
+    }
+
+}
